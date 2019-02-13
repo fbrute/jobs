f3066882fa6e9742a36ca9e0649ba8eac6e63968
France-Nor Brute <fbrute@gmail.com>
2019-02-13 16:22:54 +0100

    Fresh install and simple welcome message

---
+++ environment/captain_challenge/config/routes.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+# Routes provided by the application
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'application#hello'
+end
